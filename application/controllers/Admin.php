7dd3374f341a20a71b679ee497be94c311f8dd5d
gouxiaojing <2213998689@qq.com>
2017-11-17 17:20:29 +0800

    admin修改

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -18,8 +18,9 @@ function captcha(){
    $this->load->view('index');
   }
  }
-    //121323
+
  function show_captcha(){ //此方法用于显示验证码图片，归一个view中的img的src调用
   $this->authcode->show();
+     //121323
  }
 }
